7b6d968d16bb6d8cbdb8cc2d75b9fe72c1421d13
Maksim <maxim2000shev@gmail.com>
2022-02-10 11:31:41 +0300

    Init commit

--- main.go
+++ main.go
@@ -5,7 +5,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+var (
+	port string = "8080"
 )
 
 //Struct for representation total slice
@@ -72,6 +81,13 @@ type Country struct {
 	Code    uint64 `json:"name"`
 	Country string `json:"slug"`
 }
+type ErrorMessage struct {
+	Message string `json:"message"`
+}
+
+var (
+	db []Company
+)
 
 //Функция для распечатывания User
 func PrintCompany(c *Company) {
@@ -90,20 +106,113 @@ func PrintWarehoouse(w *Warehouse) {
 	fmt.Printf("Address: %s\n", w.Address)
 }
 
+func FindCompanyById(id uint64) (Company, bool) {
+	var company Company
+	var found bool
+	for _, p := range db {
+		if p.ID == id {
+			company = p
+			found = true
+			break
+		}
+	}
+
+	return company, found
+}
+
+func SearchCompanies(str string) ([]Company, bool) {
+	var companies []Company
+	var found bool
+
+	var sel := "SELECT * FROM companies WHERE slug LIKE str"
+	result = db.query(sel)
+
+	for _, p := range db {
+		if strings.Contains(p.Slug, str) {
+			companies = append(companies, p)
+			found = true
+		}
+	}
+
+	return companies, found
+}
+
 //1. Рассмотрим процесс десериализации (то есть когда из последовательности в объект)
-func main() {
-	//1. Создадим файл дескриптор
+
+func GetAllCompanies(writer http.ResponseWriter, request *http.Request) {
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(db)
+}
+
+func GetFoundCompany(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(request)
+
+	if vars["str"] == "" {
+		writer.WriteHeader(400)
+		error := ErrorMessage{
+			Message: "Not string fo search",
+		}
+		json.NewEncoder(writer).Encode(error)
+		return
+	}
+
+	companies, ok := SearchCompanies(vars["str"])
+
+	if ok {
+		writer.WriteHeader(200)
+		json.NewEncoder(writer).Encode(companies)
+		return
+	} else {
+		writer.WriteHeader(404)
+		error := ErrorMessage{
+			Message: "Companies not found",
+		}
+		json.NewEncoder(writer).Encode(error)
+		return
+	}
+}
+
+func GetCompanyById(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(request)
+	id, err := strconv.ParseUint(vars["id"], 0, 64)
+	if err != nil {
+		writer.WriteHeader(400)
+		error := ErrorMessage{
+			Message: "Id is not integer",
+		}
+		json.NewEncoder(writer).Encode(error)
+		return
+	}
+
+	company, ok := FindCompanyById(id)
+
+	if ok {
+		writer.WriteHeader(200)
+		json.NewEncoder(writer).Encode(company)
+		return
+	} else {
+		writer.WriteHeader(404)
+		error := ErrorMessage{
+			Message: "Company not found",
+		}
+		json.NewEncoder(writer).Encode(error)
+		return
+	}
+
+}
+
+func init() {
+
+	var companies Companies
 	jsonFile, err := os.Open("companies.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer jsonFile.Close()
-	fmt.Println("File descriptor successfully created!")
-
-	//2. Теперь десериализуем содержимое jsonFile в экземпляр Go
-	// Инициализируем экземпляр Users
-	var companies Companies
-	var warehouses Warehouses
 	// Вычитываем содержимое jsonFile в ВИДЕ ПОСЛЕДОВАТЕЛЬНОСТИ БАЙТ!
 	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
@@ -113,12 +222,18 @@ func main() {
 	// Теперь задача - перенести все из byteValue в users - это и есть десериализация!
 	json.Unmarshal(byteValue, &companies)
 	for _, c := range companies.Companies {
-		fmt.Println("================================")
-		PrintCompany(&c)
+		db = append(db, c)
+
+		//PrintCompany(&c)
 	}
-	json.Unmarshal(byteValue, &warehouses)
-	for _, w := range warehouses.Warehouses {
-		fmt.Println("================================")
-		PrintWarehoouse(&w)
-	}
+}
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/companies", GetAllCompanies).Methods("GET")
+	router.HandleFunc("/companies/{id}", GetCompanyById).Methods("GET")
+	router.HandleFunc("/companies/found/{str}", GetFoundCompany).Methods("GET")
+	log.Println("Router gonfigured sucsessfully! Let's Go! ")
+	log.Fatal(http.ListenAndServe(":"+port, router))
+
 }
